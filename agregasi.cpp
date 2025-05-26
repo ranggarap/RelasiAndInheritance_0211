442011b3e035f978c9a4021402c39b13ed72949d
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 18:46:16 +0700

    membuat fungsi utama

--- agregasi.cpp
+++ agregasi.cpp
@@ -4,3 +4,12 @@ using namespace std;
 
 #include "anak.h"
 #include "ibu.h"
+
+int main()
+{
+    ibu *varibu = new ibu ("dini");
+    ibu *varibu2 = new ibu ("novi");
+    anak *varanak1 =new anak("tono");
+    anak *varanak2 =new anak("rini");
+    anak *varanak3 =new anak("dewi");
+}
