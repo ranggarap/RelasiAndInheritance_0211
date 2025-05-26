3d823444867a4032b2dd4b8dbf238479c094caec
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 18:21:34 +0700

    memberikan hak akses public ke string nama

--- anak.h
+++ anak.h
@@ -1,3 +1,15 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+class anak{
+    public:
+        string nama;
+        anak(string pnama)  : (pNama)
+        {
+            cout << "anak\""<<nama <<"\" ada \n"; 
+        }
+        ~anak()
+        {
+            cout << "anak\"" << nama << "\" tidak ada\n";
+        }
+        };
