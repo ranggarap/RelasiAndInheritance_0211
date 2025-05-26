f36757fa88cc05c07c7c8082e9379a179011ac22
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 18:59:40 +0700

    lanjutan dari fungsi utama

--- agregasi.cpp
+++ agregasi.cpp
@@ -12,4 +12,19 @@ int main()
     anak *varanak1 =new anak("tono");
     anak *varanak2 =new anak("rini");
     anak *varanak3 =new anak("dewi");
+
+    varibu-> tambahanak(varanak1);
+    varibu-> tambahanak(varanak2);
+    varibu2-> tambahanak(varanak3);
+    varibu2-> tambahanak(varanak1);
+
+    varibu->cetakanak();
+    varibu2->cetakanak();
+
+    delete varibu;
+    delete varibu2;
+    delete varanak1;
+    delete varanak2;
+    delete varanak3;
+    return 0;
 }
