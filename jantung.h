ef078aa8362718cb68914cbbb818895209aecec9
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 21:14:27 +0700

    Menambahkan kelas jantung dengan konstruktor dan destruktor sederhana

---
+++ jantung.h
@@ -0,0 +1,15 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public :
+    jantung() {
+        cout << "jantung dihidupkaan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
