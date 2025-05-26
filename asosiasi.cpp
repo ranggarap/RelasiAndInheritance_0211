fc0ffa48f32b6679a96a07dbc024f8c7864e37d3
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 20:51:41 +0700

    menambah data public dan int

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -19,3 +19,22 @@ public :
         }
 };
 
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+    }
+};
+
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
