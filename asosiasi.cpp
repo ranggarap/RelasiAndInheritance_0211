e6dbfb53b3cb5916bbf974be42e99790b5341f5a
ranggarap <fadhilahrangga26@gmail.com>
2025-05-26 21:08:56 +0700

    Menambahkan fungsi main untuk menguji hubungan antara dokter dan pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -53,9 +53,24 @@ void dokter :: cetakPasien() {
 }
 
 int main() {
-    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
-    cout << siswa1.perkenalan();
-    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
 
     return 0;
 }
